521a27aa9ba0108c45b3d4026905484ebdcc87d7
ashneeldas2 <adas2@stuy.edu>
2017-10-18 11:47:24 -0400

    confused

--- list.c
+++ list.c
@@ -24,21 +24,31 @@ song_node * insert_front(song_node * n, char * name, char * artist){
 	head->next = n;
 	strcpy(head->name, name);
 	strcpy(head->artist, artist);
+	//printf("%s\n", name);
 	return head;
 }
 
 song_node * insert_ordered(song_node * n, char * name, char * artist){
-	song_node *head = (song_node *)malloc(sizeof(song_node));
-	
-	while(n){
-		if(strcmp(n->name, name) > 0){
-			br();
-			n->next = insert_front(n, name, artist);
-	//		print_list(n);
-			break;
-		}
-		n = n->next;	
+	song_node * temp = n;
+	song_node * tmp2 = n -> next;
+	song_node * head = n; 	
+	while(temp && (strcmp(temp->name, name) < 0)){
+		temp = temp->next;	
 	}
+	head = insert_front(temp, name, artist);
+//	print_list(n);
+	br();
+	while (strcmp(n->name, name) < 0 && n) {
+		printf("%d\n", strcmp(n->name, name));		
+		n = n-> next;
+	}   
+	print_list(n);
+	br();
+	print_list(head);
+	n -> next = head;
+	head->next = tmp2;
+	br();
+	//print_list(n);
 	return n;
 }
 song_node * free_list(song_node * n){
@@ -53,13 +63,12 @@ song_node * free_list(song_node * n){
 int main(){
 	song_node *head;
 	song_node *next = 0;
-	head = insert_front(next, "c", "b");
-	head = insert_front(head, "a", "b");
-	head = insert_ordered(head, "b", "b");
-	
+	head = insert_front(next, "m", "b");
+	head = insert_front(head, "b", "b");
+	head = insert_front(head, "a", "d");
+	head = insert_ordered(head, "c", "b");	
 	br();
 	//printf("%d\n", head->next->i);
 	print_list(head);
 	free_list(head);
-	//print_kist(head);
 }

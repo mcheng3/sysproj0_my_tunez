f55dfd486bf3d85ebae5b3680d752bfec8b4d78f
ashneeldas2 <adas2@stuy.edu>
2017-10-20 21:21:29 -0400

    updated list.h

--- list.h
+++ list.h
@@ -6,8 +6,16 @@ typedef struct song_node{
 void print_list(song_node *);
 song_node * insert_front(song_node *, char*, char*);
 song_node * free_list(song_node *);
+int comp_song(song_node *, char *, char *);
 song_node * insert_ordered(song_node *, char*, char*);
 song_node * find_song(song_node *, char*, char*);
 song_node * find_song_artist(song_node *, char*);
 song_node * rand_node(song_node *);
 song_node * remove_node(song_node *, char *, char *);
+// table functions
+void add_song(song_node * ary[], char *, char *);
+song_node * search_song(song_node * ary[], char *, char *);
+song_node * search_artist(song_node * ary[], char *);
+void print_letter_list(song_node * ary[], char);
+  
+  
